37de49d355c3c62338ca3e55f98e13c3c94effac
Tan Ngo <tan_ngo@datahouse.com>
2026-02-01 21:07:47 +0700

    Update comment in Home component from 'test 2' to 'test 3'

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 
-//test 2
+//test 3
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
